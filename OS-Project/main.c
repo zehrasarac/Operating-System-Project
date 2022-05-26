18b65fd64972bf0690a26d73bd58ffa55e9a9ce0
zehrasarac <zehrassarac@gmail.com>
2022-05-26 23:39:59 +0300

    2.Teslim

--- OS-Project/main.c
+++ OS-Project/main.c
@@ -1,34 +1,125 @@
-//#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include "surecbasladi.h"
-#include "surecbitti.h"
-#include "sistemcagrisi.h"
-#include "zamanlayicikesmesi.h"
 
-enum olaylar {e1,e2,e3,e4}; 
-int olayMiktar = 4;
+void sistemcagrisi(){
+    printf("Sistem Cagrisi\n");
+}
 
-int main(int argc, char const *argv[])
-{
-    enum olaylar olay[olayMiktar];
+void surecbasladi(){
+    char *s1 = "Surec Basladi";
+    printf("%s\n",s1);
+}
 
-    const char *olayN[] = {"surec basladi","surec bitti","sistem cagrisi","zamanlayici kesmesi"};
+void surecbitti(){
+    printf("Surec Bitti\n");
+}
 
-    srand(time(NULL));
-    for (int i = 0; i < 4; i++)
-    {
-        clock_t tic = clock();
-        olay[i] = rand() % 4;
-        printf("%d. Olay ",olay[i]);
-        printf("%s gerceklesti\n", olayN[olay[i]]);
-        clock_t toc = clock();
-        printf("%f saniye sürdü\n", (double)(toc - tic) / CLOCKS_PER_SEC);
-    }
-    
-    return 0;
+void zamanlayicikesmesi(){
+    printf("Zamanlayici Kesmesi\n");
 }
 
 
+void beklemeSuresi(int olaylar[], int n,
+						int bt[], int wt[])
+{
 
+	wt[0] = 0;
+
+
+	for (int i = 1; i < n ; i++ )
+		wt[i] = bt[i-1] + wt[i-1] ;
+}
+
+
+void donusSuresi( int olaylar[], int n,
+				int bt[], int wt[], int tat[])
+{
+
+	for (int i = 0; i < n ; i++)
+		tat[i] = bt[i] + wt[i];
+}
+
+
+void ortalamaSure( int olaylar[], int n, int bt[])
+{
+	int wt[n], tat[n], total_wt = 0, total_tat = 0;
+
+
+	beklemeSuresi(olaylar, n, bt, wt);
+
+
+	donusSuresi(olaylar, n, bt, wt, tat);
+
+
+	printf("Surec Calisma Suresi Bekleme Suresi Geri Dönüş Suresi\n");
+
+
+	for (int i=0; i<n; i++)
+	{
+		total_wt = total_wt + wt[i];
+		total_tat = total_tat + tat[i];
+		printf(" %d ",(i+1));
+		printf("	 	        %d ", bt[i] );
+		printf("	 	        %d",wt[i] );
+		printf("	 	        %d\n",tat[i] );
+	}
+	int s=(float)total_wt / (float)n;
+	int t=(float)total_tat / (float)n;
+	printf("Ortalama Bekleme Süresi = %d",s);
+	printf("\n");
+	printf("Ortalama Geri Dönüş Süresi = %d ",t);
+}
+void rastgeleOlay(int olaylar[], int olay_sayisi, int olay_tipi_s){
+	srand(time(NULL));
+
+	for (int i = 0; i < olay_sayisi; i++){
+		olaylar[i] = rand() % olay_tipi_s;
+	}
+}
+
+void olayCagir(int olayTipi){
+	switch (olayTipi)
+	{
+	case 1:
+		surecbasladi();
+		break;
+	case 2:
+		sistemcagrisi();
+		break;	
+	case 3:
+		surecbitti();
+		break;
+	
+	case 4:
+		zamanlayicikesmesi();
+		break;
+	}
+}
+
+
+int main()
+{
+
+	int olay_sayisi = 5;
+    int olay_tipi_s = 4;
+
+    
+    int olaylar[olay_sayisi];
+	rastgeleOlay(olaylar,olay_sayisi,olay_tipi_s);
+
+    for (int i=0;i<4;i++){
+		olayCagir(olaylar[i]);
+	}
+
+	
+	int n = sizeof olaylar / sizeof olaylar[0];
+
+	int donus_suresi[] = {10, 5, 8,3,6};
+
+	ortalamaSure(olaylar, n, donus_suresi);
+
+
+
+	return 0;
+}
